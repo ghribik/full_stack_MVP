42a0293ab6d5fd565c7601baa776ddccbd84a902
ghribik <g.hribik@gmail.com>
2022-11-14 22:41:45 -0800

    CSS and HTML styling update

--- front-end/public/index.js
+++ front-end/public/index.js
@@ -36,12 +36,12 @@ const read = document.getElementById('read').addEventListener("click", event => 
                 Playlist: ${song.playlist_id}`
                 content.appendChild(songElement)
             }else{
-                alert("Song data parameters missing or improperly formatted!");
+                alert("Reading of song data from server failed!");
             };
         })
         .catch(error => {
             console.error(error);
-            alert("Song data parameters missing or improperly formatted!");
+            alert("Song ID number missing or improperly formatted!");
         })
     };
 });
@@ -74,14 +74,17 @@ const create = document.getElementById('create').addEventListener("click", event
             Title: ${song.title}, 
             Artist: ${song.artist}, 
             Album: ${song.album}, 
-            Track:${song.track_num}, 
+            Track:${song.track_num},
             Playlist: ${song.playlist_id}`
             content.appendChild(songElement)
         }else{
-            alert("Song data parameters missing or improperly formatted!");
+            alert("Reading of created song data from server failed!");
         };
     })
-    .catch(err => console.error(err))
+    .catch(error => {
+        console.error(error);
+        alert("Song data parameters missing or improperly formatted!");
+    })
 });
 
 //PATCH --- Update data for a specific song by id
@@ -114,10 +117,13 @@ const patch = document.getElementById('update').addEventListener("click", event 
             Playlist: ${song.playlist_id}`;
             content.appendChild(songElement);
         }else{
-            alert("Song data parameters missing or improperly formatted!");
+            alert("Reading of updated song data from server failed!");
         };
     })
-    .catch(err => console.error(err));
+    .catch(error => {
+        console.error(error);
+        alert("Song data parameters missing or improperly formatted!");
+    })
 });
 
 //DELETE --- Delete a song from the database by id
@@ -135,10 +141,57 @@ const deleteSong = document.getElementById('delete').addEventListener("click", e
              alert("Song ID not found or improperly formatted!", response);
          };
      })
-     .catch(err => console.error(err))
+     .catch(error => {
+        console.error(error);
+        alert("Song ID number missing or improperly formatted!");
+    })
  });
 
  function clearBox(elementID)
 {
     document.getElementById(elementID).innerHTML = "";
 }
+
+const searchButton = document.getElementById('search').addEventListener("click", event => {
+    let queryInput = document.getElementById('query').value;
+    console.log(queryInput);
+    event.preventDefault();
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '76af4ca374mshf7643492cfe0a22p15555djsna0d4900b33d3',
+            'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        }
+    };
+    
+    fetch(`https://shazam.p.rapidapi.com/search?term=${queryInput}&locale=en-US&offset=0&limit=6`, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.length);
+            console.log(response.tracks.hits[0].track.share.subject);
+            console.log (response.tracks.hits[0].track.images.coverart);
+            createCard(response)
+        })
+        .catch(err => console.error(err));
+})
+
+function createCard(response) {
+    for(let i = 0; i < 6; i ++){
+        let songCard = document.createElement('div');
+        songCard.setAttribute("class", "card");
+        songCard.style.float = "left";
+        let cardImg = document.createElement('img');
+        cardImg.setAttribute("src", response.tracks.hits[i].track.images.coverart);
+        cardImg.setAttribute("alt", "Avatar")
+        cardImg.style.width= "300px";
+        songCard.appendChild(cardImg);
+        let cardContainer = document.createElement('div');
+        cardContainer.setAttribute("class", "container")
+        songCard.appendChild(cardContainer);
+        let cardTitle = document.createElement('h4');
+        cardTitle.innerHTML = response.tracks.hits[i].track.share.subject;
+        cardContainer.appendChild(cardTitle);
+        let footer = document.getElementById('footer');
+        footer.appendChild(songCard);
+    };
+};
